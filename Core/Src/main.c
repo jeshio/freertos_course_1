ab6abe20778ac64c620638ccdc33739d8b220685
jeshio <jeshio@yandex.ru>
2025-02-25 01:13:07 +0500

    adds tasks debug load

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -56,10 +56,10 @@ osThreadId myTask04Handle;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
-void StartTask01(void const *argument);
-void StartTask03(void const *argument);
-void StartTask02(void const *argument);
-void StartTask04(void const *argument);
+void StartTask01(void const * argument);
+void StartTask03(void const * argument);
+void StartTask02(void const * argument);
+void StartTask04(void const * argument);
 
 /* USER CODE BEGIN PFP */
 
@@ -71,9 +71,9 @@ void StartTask04(void const *argument);
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
 
@@ -105,7 +105,7 @@ int main(void)
   /* Define the tasks */
   osThreadDef(myTask01, StartTask01, osPriorityNormal, 0, 128);
   osThreadDef(myTask02, StartTask02, osPriorityNormal, 0, 128);
-  osThreadDef(myTask03, StartTask03, osPriorityNormal, 0, 128);
+  osThreadDef(myTask03, StartTask03, osPriorityHigh, 0, 128);
   osThreadDef(myTask04, StartTask04, osPriorityNormal, 0, 128);
 
   /* Start the tasks */
@@ -116,30 +116,30 @@ int main(void)
 
   /* USER CODE END 2 */
 
-  /* USER CODE BEGIN Header */
-  /**
-   ******************************************************************************
-   * @file           : main.c
-   * @brief          : Main program body
-   ******************************************************************************
-   * @attention
-   *
-   * Copyright (c) 2025 STMicroelectronics.
-   * All rights reserved.
-   *
-   * This software is licensed under terms that can be found in the LICENSE file
-   * in the root directory of this software component.
-   * If no LICENSE file comes with this software, it is provided AS-IS.
-   *
-   ******************************************************************************
-   */
-  /* USER CODE END Header */
-  /**
-   * @}
-   */
-  /**
-   * @}
-   */
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/**
+* @}
+*/
+/**
+* @}
+*/
 
   /* Start scheduler */
   osKernelStart();
@@ -158,17 +158,17 @@ int main(void)
 }
 
 /**
- * @brief System Clock Configuration
- * @retval None
- */
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
-   * in the RCC_OscInitTypeDef structure.
-   */
+  * in the RCC_OscInitTypeDef structure.
+  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -182,8 +182,9 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-   */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1;
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -195,10 +196,10 @@ void SystemClock_Config(void)
 }
 
 /**
- * @brief USART2 Initialization Function
- * @param None
- * @retval None
- */
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_USART2_UART_Init(void)
 {
 
@@ -226,18 +227,19 @@ static void MX_USART2_UART_Init(void)
   /* USER CODE BEGIN USART2_Init 2 */
 
   /* USER CODE END USART2_Init 2 */
+
 }
 
 /**
- * @brief GPIO Initialization Function
- * @param None
- * @retval None
- */
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-  /* USER CODE BEGIN MX_GPIO_Init_1 */
-  /* USER CODE END MX_GPIO_Init_1 */
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
@@ -246,10 +248,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LD2_Pin | GPIO_PIN_8 | GPIO_PIN_10, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LD2_Pin|GPIO_PIN_8|GPIO_PIN_10, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
@@ -258,21 +260,21 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : LD2_Pin PA8 PA10 */
-  GPIO_InitStruct.Pin = LD2_Pin | GPIO_PIN_8 | GPIO_PIN_10;
+  GPIO_InitStruct.Pin = LD2_Pin|GPIO_PIN_8|GPIO_PIN_10;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PB10 PB3 PB4 PB5 */
-  GPIO_InitStruct.Pin = GPIO_PIN_10 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5;
+  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /* USER CODE BEGIN MX_GPIO_Init_2 */
-  /* USER CODE END MX_GPIO_Init_2 */
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
@@ -339,7 +341,7 @@ void vApplicationIdleHook(void)
  * @retval None
  */
 /* USER CODE END Header_StartTask01 */
-void StartTask01(void const *argument)
+void StartTask01(void const * argument)
 {
   /* USER CODE BEGIN 5 */
 #ifdef TASK_DEBUG
@@ -349,6 +351,11 @@ void StartTask01(void const *argument)
   /* Infinite loop */
   for (;;)
   {
+    // нагрузка для увеличения времени выполнения задачи
+    for (int i = 0; i < 0x2FF; i++)
+    {
+      __NOP();
+    }
     osDelay(1);
   }
   /* USER CODE END 5 */
@@ -361,7 +368,7 @@ void StartTask01(void const *argument)
  * @retval None
  */
 /* USER CODE END Header_StartTask03 */
-void StartTask03(void const *argument)
+void StartTask03(void const * argument)
 {
   /* USER CODE BEGIN StartTask03 */
 #ifdef TASK_DEBUG
@@ -370,6 +377,11 @@ void StartTask03(void const *argument)
   /* Infinite loop */
   for (;;)
   {
+    // нагрузка для увеличения времени выполнения задачи
+    for (int i = 0; i < 0x1FF; i++)
+    {
+      __NOP();
+    }
     osDelay(1);
   }
   /* USER CODE END StartTask03 */
@@ -382,7 +394,7 @@ void StartTask03(void const *argument)
  * @retval None
  */
 /* USER CODE END Header_StartTask02 */
-void StartTask02(void const *argument)
+void StartTask02(void const * argument)
 {
   /* USER CODE BEGIN StartTask02 */
 #ifdef TASK_DEBUG
@@ -391,6 +403,11 @@ void StartTask02(void const *argument)
   /* Infinite loop */
   for (;;)
   {
+    // нагрузка для увеличения времени выполнения задачи
+    for (int i = 0; i < 0x1FF; i++)
+    {
+      __NOP();
+    }
     osDelay(1);
   }
   /* USER CODE END StartTask02 */
@@ -403,7 +420,7 @@ void StartTask02(void const *argument)
  * @retval None
  */
 /* USER CODE END Header_StartTask04 */
-void StartTask04(void const *argument)
+void StartTask04(void const * argument)
 {
   /* USER CODE BEGIN StartTask04 */
 #ifdef TASK_DEBUG
@@ -412,38 +429,41 @@ void StartTask04(void const *argument)
   /* Infinite loop */
   for (;;)
   {
-    osDelay(1);
+    // нагрузка для увеличения времени выполнения задачи
+    for (int i = 0; i < 0x2FFF; i++)
+    {
+      __NOP();
+    }
+    vTaskDelay(pdMS_TO_TICKS(3));
   }
   /* USER CODE END StartTask04 */
 }
 
 /**
- * @brief  Period elapsed callback in non blocking mode
- * @note   This function is called  when TIM1 interrupt took place, inside
- * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
- * a global variable "uwTick" used as application time base.
- * @param  htim : TIM handle
- * @retval None
- */
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM1 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
   /* USER CODE BEGIN Callback 0 */
   /* USER CODE END Callback 0 */
-  if (htim->Instance == TIM1)
-  {
-    // отмечаем системный тик для логического анализатора
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_SET); // Установить пин перед тиком
+  if (htim->Instance == TIM1) {
     HAL_IncTick();
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_RESET); // Сбросить пин после тика
   }
   /* USER CODE BEGIN Callback 1 */
+  GPIOA->BSRR = GPIO_PIN_10;
+  GPIOA->BSRR = (uint32_t)GPIO_PIN_10 << 16U;
   /* USER CODE END Callback 1 */
 }
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -455,14 +475,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef USE_FULL_ASSERT
+#ifdef  USE_FULL_ASSERT
 /**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
